40d679d6316703693c65d1702cd9f9a4fae7ed23
sozanmahmoud14-five <sozanmahmoud821@gmail.com>
2026-02-18 23:26:38 +0200

    add splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -2,14 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:hormo_track/core/constants/app_routes.dart';
 import 'features/onboarding/view/onBoarding_screen.dart';
 import 'features/onboarding/view/splash_screen.dart';
-
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
