6fff20764df10b27247ec7cd56d426799a006d9e
sozanmahmoud14-five <sozanmahmoud821@gmail.com>
2026-02-20 01:05:49 +0200

    add log in and sign up screens

---
+++ lib/features/auth/view/login_screen.dart
@@ -0,0 +1,214 @@
+import 'package:flutter/material.dart';
+import 'package:hormo_track/core/constants/app_assets.dart';
+import 'package:hormo_track/core/constants/app_colors.dart';
+import 'package:hormo_track/core/constants/app_strings.dart';
+import 'package:hormo_track/features/auth/view/signup_screen.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  bool rememberMe = false;
+  bool obscurePassword = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      body: Stack(
+        children: [
+          ///  Background
+          Positioned.fill(
+            child: Image.asset(
+              AppAssets.authBackgroundImage,
+              fit: BoxFit.cover,
+            ),
+          ),
+
+          SafeArea(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: Column(
+                children: [
+                  const SizedBox(height: 100),
+
+                  /// Title
+                  const Text(
+                    AppStrings.login,
+                    style: TextStyle(
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold,
+                      color: AppColors.header,
+                    ),
+                  ),
+
+                  const SizedBox(height: 45),
+
+                  /// Email
+                  _buildField(
+                    hint: AppStrings.email,
+                    icon: Icons.email_rounded,
+                  ),
+
+                  const SizedBox(height: 20),
+
+                  /// Password
+                  _buildField(
+                    hint: AppStrings.password,
+                    icon: Icons.lock_rounded,
+                    isPassword: true,
+                  ),
+
+                  const SizedBox(height: 15),
+
+                  /// Remember + Forgot
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Row(
+                        children: [
+                          Checkbox(
+                            value: rememberMe,
+                            onChanged: (value) {
+                              setState(() {
+                                rememberMe = value!;
+                              });
+                            },
+                            activeColor: AppColors.buttoColor,
+                          ),
+                          const Text(
+                            AppStrings.rememberMe,
+                            style: TextStyle(
+                              color: AppColors.header,
+                              fontSize: 14,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ],
+                      ),
+
+                      TextButton(
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (_) => const ChangePasswordScreen(),
+                            ),
+                          );
+                        },
+                        child: const Text(
+                          AppStrings.forgotPassword,
+                          style: TextStyle(
+                            color: AppColors.header,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+
+                  const SizedBox(height: 40),
+
+                  /// Login Button
+                  SizedBox(
+                    width: 200,
+                    height: 55,
+                    child: ElevatedButton(
+                      onPressed: () {
+                        Navigator.pushReplacement(
+                          context,
+                          MaterialPageRoute(builder: (_) => const BeforeHome()),
+                        );
+                      },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: AppColors.buttoColor,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(30),
+                        ),
+                      ),
+                      child: const Text(
+                        AppStrings.login,
+                        style: TextStyle(fontSize: 20, color: Colors.white),
+                      ),
+                    ),
+                  ),
+
+                  const SizedBox(height: 35),
+
+                  /// Sign Up
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Text(
+                        AppStrings.noAccount,
+                        style: TextStyle(
+                          color: Colors.grey,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      const SizedBox(width: 3),
+                      TextButton(
+                        onPressed: () {
+                          Navigator.pushReplacement(
+                            context,
+                            MaterialPageRoute(
+                              builder: (_) => const SignUpScreen(),
+                            ),
+                          );
+                        },
+                        child: const Text(
+                          AppStrings.signup,
+                          style: TextStyle(
+                            color: AppColors.header,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildField({
+    required String hint,
+    required IconData icon,
+    bool isPassword = false,
+  }) {
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(30),
+        boxShadow: [
+          BoxShadow(
+            color: AppColors.buttoColor.withOpacity(0.25),
+            blurRadius: 20,
+            offset: const Offset(0, 10),
+          ),
+        ],
+      ),
+      child: TextField(
+        obscureText: isPassword ? obscurePassword : false,
+        decoration: InputDecoration(
+          prefixIcon: Icon(icon, color: const Color.fromARGB(150, 0, 0, 0)),
+          hintText: hint,
+          filled: true,
+          fillColor: const Color(0xffFFFFFF),
+          contentPadding: const EdgeInsets.symmetric(vertical: 18),
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(30),
+            borderSide: BorderSide.none,
+          ),
+        ),
+      ),
+    );
+  }
+}
