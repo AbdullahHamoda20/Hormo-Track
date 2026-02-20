a869969293f35b88fc50772cc644e41291ec525b
aliaaabdelbaky05 <aliaaabdelbaky05@gmail.com>
2026-02-20 21:21:18 +0200

    add tips screen

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'features/settings/view/about_us_screen.dart';
 import 'features/settings/view/profile_screen.dart';
 import 'features/settings/view/contact_screen.dart';
 import 'features/settings/view/settings_screen.dart';
+import 'features/tips/view/tips_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,7 +18,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       // 
-      home:  SettingsScreen(),
+      home:  TipsScreen(),
         
     );
   }
