f061e175dd5c221f4e2d0a26ea4d5ef0ac6fe884
aliaaabdelbaky05 <aliaaabdelbaky05@gmail.com>
2026-05-07 19:08:46 +0300

    add tips_screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,6 @@
 import 'package:flutter/material.dart';
-
-
-import 'features/settings/view/about_us_screen.dart';
-import 'features/settings/view/profile_screen.dart';
-import 'features/settings/view/contact_screen.dart';
-import 'features/settings/view/settings_screen.dart';
-import 'features/tips/view/tips_screen.dart';
-import 'features/auth/view/change_password_screen.dart';
-
-import 'package:hormo_track/core/constants/app_routes.dart';
-import 'features/onboarding/view/onBoarding_screen.dart';
-import 'features/onboarding/view/splash_screen.dart';
-
-
+// تأكدي من عمل import للملف اللي فيه صفحة الـ tips
+import 'features/tips/view/tips_screen.dart'; 
 
 void main() {
   runApp(const MyApp());
@@ -21,21 +9,12 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-
- @override
-
-
-
- @override
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Password UI',
-     
-      home: const ChangePasswordScreen(),
+   
+      home: const PcosTipsScreen(), 
     );
   }
 }
